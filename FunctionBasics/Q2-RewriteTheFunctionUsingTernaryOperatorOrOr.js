38224299299767a8629593528310457e7ef3b489
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-17 17:04:42 -0800

    Created Q2

---
+++ FunctionBasics/Q2-RewriteTheFunctionUsingTernaryOperatorOrOr.js
@@ -0,0 +1,19 @@
+ //#2 Rewrite the function using '?' or '||'
+//   The following function returns true if the parameter age is 
+//   greater than 18.
+//   Otherwise it asks for a confirmation and returns its result.
+  
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('Did parents allow you?');
+    }
+  }
+//   Rewrite it, to perform the same, but without if, in a single line.
+//   Make two variants of checkAge:  
+//   Using a question mark operator ?
+let checkAge = age => age > 18 ? true : confirm('Did parents allow you?') 
+
+//   Using OR ||
+let checkAge = age => age > 18 || confirm('Did parents allow you?')
