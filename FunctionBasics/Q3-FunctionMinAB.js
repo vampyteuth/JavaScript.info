e2c4408219cac8a7c78a9322acba0c22f79ffaff
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-17 17:05:21 -0800

    Created Q3

---
+++ FunctionBasics/Q3-FunctionMinAB.js
@@ -0,0 +1,12 @@
+// //#3Function min(a,b)
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+// For instance:
+
+// min(2, 5) == 2
+// min(3, -1) == -1
+// min(1, 1) == 1
+
+function min(a,b) {
+    return a < b ? a : b
+}
