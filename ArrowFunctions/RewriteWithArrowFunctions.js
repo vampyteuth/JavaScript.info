6205791d47215371295f0ba993c517a9eb641dec
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-17 17:36:17 -0800

    Created Q1-Rewrite

---
+++ ArrowFunctions/RewriteWithArrowFunctions.js
@@ -0,0 +1,26 @@
+//Replace Function Expressions with arrow functions in the code below:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+
+//my solution
+
+ let ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask( 
+    "Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+  );
